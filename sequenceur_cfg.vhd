3318f04a1ce57857377eeabe42625758dfc54cb4
Tom Compagnon <tom.compagnon2003@gmail.com>
2025-10-08 12:09:17 +0200

    4archi sequenceur

--- sequenceur_cfg.vhd
+++ sequenceur_cfg.vhd
@@ -6,8 +6,10 @@ USE work.my_package.all  ;
 CONFIGURATION cfg_sequenceur OF sequenceur_tb IS 
     FOR sequenceur_tb_arch
         FOR DUT : sequenceur USE ENTITY 
-            --work.sequenceur(archi_Moore);
-            work.sequenceur(archi_Mealy);
+        work.sequenceur(archi_Moore_sync);
+            --work.sequenceur(archi_Moore_imm);
+            --work.sequenceur(archi_Mealy_sync);
+            --work.sequenceur(archi_Mealy_imm);
         END FOR;
     END FOR;
 END cfg_sequenceur ;
